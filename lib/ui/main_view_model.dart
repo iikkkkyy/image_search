437f2e6e88bdf92f38ada0617a6640bb84c2b656
kimkangtae <tt997768@gmail.com>
2024-01-02 19:41:13 +0900

    Feat: stream getter 추가

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -9,8 +9,10 @@ class MainViewModel {
   final _repository = ImageRepository();
 
   List<ImageModel> _items = [];
-
   final _isLoading = StreamController<bool>();
+
+  List<ImageModel> get items => List.unmodifiable(_items);
+  get isLoading => _isLoading.stream;
 
   Future<void> searchImage(String query) async {
     _isLoading.add(true);
