bcb5e5b3261544a7ffe881bc03db05ffda623f44
kimkangtae <tt997768@gmail.com>
2024-01-02 19:11:44 +0900

    Feat: main screen textField 및 Iconbutton 추가

--- lib/ui/widget/main_screen.dart
+++ lib/ui/widget/main_screen.dart
@@ -8,8 +8,36 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextField(
+                  controller: myController,
+                  decoration:  InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Search',
+                    suffixIcon: IconButton(
+                      icon : Icon(Icons.search),
+                      onPressed: () {},),
+
+                  ),
+                ),
+              ),
+
+              const SizedBox(width: 10,),
+
+
+
+            ],
+
+      )),
+    );
   }
 }
