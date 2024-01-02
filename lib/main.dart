8d7d0639b73816477d593ae42144b543c736550e
kimkangtae <tt997768@gmail.com>
2024-01-02 19:21:34 +0900

    Refactor: dark모드로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        primaryColor: Colors.white,
       ),
+      darkTheme: ThemeData.dark(),
       home: const MainScreen(),
     );
   }
