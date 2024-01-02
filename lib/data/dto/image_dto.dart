b9f929f109f73f4c691459d152b2b85a3e097749
kimkangtae <tt997768@gmail.com>
2024-01-02 17:42:12 +0900

    refactor: add nullable

--- lib/data/dto/image_dto.dart
+++ lib/data/dto/image_dto.dart
@@ -33,52 +33,52 @@ class PixabayResponse {
 }
 
 class PixabayImage {
-  int id;
-  String pageURL;
-  String type;
-  String tags;
-  String previewURL;
-  int previewWidth;
-  int previewHeight;
-  String webformatURL;
-  int webformatWidth;
-  int webformatHeight;
-  String largeImageURL;
-  int imageWidth;
-  int imageHeight;
-  int imageSize;
-  int views;
-  int downloads;
-  int collections;
-  int likes;
-  int comments;
-  int userId;
-  String user;
-  String userImageURL;
+  int? id;
+  String? pageURL;
+  String? type;
+  String? tags;
+  String? previewURL;
+  int? previewWidth;
+  int? previewHeight;
+  String? webformatURL;
+  int? webformatWidth;
+  int? webformatHeight;
+  String? largeImageURL;
+  int? imageWidth;
+  int? imageHeight;
+  int? imageSize;
+  int? views;
+  int? downloads;
+  int? collections;
+  int? likes;
+  int? comments;
+  int? userId;
+  String? user;
+  String? userImageURL;
 
   PixabayImage({
-    required this.id,
-    required this.pageURL,
-    required this.type,
-    required this.tags,
-    required this.previewURL,
-    required this.previewWidth,
-    required this.previewHeight,
-    required this.webformatURL,
-    required this.webformatWidth,
-    required this.webformatHeight,
-    required this.largeImageURL,
-    required this.imageWidth,
-    required this.imageHeight,
-    required this.imageSize,
-    required this.views,
-    required this.downloads,
-    required this.collections,
-    required this.likes,
-    required this.comments,
-    required this.userId,
-    required this.user,
-    required this.userImageURL,
+    this.id,
+    this.pageURL,
+    this.type,
+    this.tags,
+    this.previewURL,
+    this.previewWidth,
+    this.previewHeight,
+    this.webformatURL,
+    this.webformatWidth,
+    this.webformatHeight,
+    this.largeImageURL,
+    this.imageWidth,
+    this.imageHeight,
+    this.imageSize,
+    this.views,
+    this.downloads,
+    this.collections,
+    this.likes,
+    this.comments,
+    this.userId,
+    this.user,
+    this.userImageURL,
   });
 
   factory PixabayImage.fromJson(Map<String, dynamic> json) {
