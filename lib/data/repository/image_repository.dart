c53649a7fdda5f8f66d66ed7152e8a5a6dc15961
kimkangtae <tt997768@gmail.com>
2024-01-03 02:38:44 +0900

    Comment: Todo 주석 제거

--- lib/data/repository/image_repository.dart
+++ lib/data/repository/image_repository.dart
@@ -3,7 +3,6 @@ import 'package:image_search_fast_coding/data/mapper/image_mapper.dart';
 import 'package:image_search_fast_coding/data/model/image_model.dart';
 
 class ImageRepository {
-  //Todo provider 변경
   final _api = ImageApi();
 
   Future<List<ImageModel>> getImageItems(String query) async {
