3e34dc1121e11b0b28e78be8f1aac6f8d8d15950
kimkangtae <tt997768@gmail.com>
2024-01-02 23:31:56 +0900

    feat: 테마 변경

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         primaryColor: Colors.white,
+        hintColor: Colors.white
       ),
       darkTheme: ThemeData.dark(),
       home: const MainScreen(),
