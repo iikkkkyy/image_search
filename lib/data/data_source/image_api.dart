45702c1c74f8fa0525b8ce722a33bf5ede74f28d
kimkangtae <tt997768@gmail.com>
2024-01-02 18:52:53 +0900

    Refactor: getImage => getImages

--- lib/data/data_source/image_api.dart
+++ lib/data/data_source/image_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:image_search_fast_coding/data/dto/image_dto.dart';
 
 class ImageApi {
-  Future<PixabayResponse> getImage(String query) async {
+  Future<PixabayResponse> getImages(String query) async {
     final response = await http.get(Uri.parse('https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo'));
     return PixabayResponse.fromJson(jsonDecode(response.body));
   }
